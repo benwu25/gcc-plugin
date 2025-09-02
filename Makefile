4b35c8076acb3ac974975d605c9bf5bc1c52cdd6
benwu25 <soggysocks206@gmail.com>
2025-09-02 16:19:46 -0700

    Determine gcc_data for PLUGIN_START_PARSE_FUNCTION and FINISH

---
+++ Makefile
@@ -0,0 +1,14 @@
+all:
+	g++ -g -fPIC -shared -I/usr/lib/gcc/x86_64-linux-gnu/13/plugin/include -L/usr/lib/gcc/x86_64-linux-gnu/13/plugin -o libGCCPlugin.so gcc-plugin.cc
+
+test: all
+	g++ -fplugin=./libGCCPlugin.so main.cpp
+
+test-debug: all
+	/home/benwu25/Documents/mirror/gcc-install/gcc-install/bin/g++ -fplugin=./libGCCPlugin.so main.cpp -wrapper gdb,--args
+
+clean:
+	rm libGCCPlugin.so
+
+clean2:
+	rm a.out
--- gcc-plugin.cc
+++ gcc-plugin.cc
@@ -1,16 +1,26 @@
 #include <gcc-plugin.h>
 #include <plugin.h>
+#include <coretypes.h>
+#include <tree.h>
+#include <iostream>
 
 int plugin_is_GPL_compatible = 0;
 
 // callbacks
 void handle_function(void *gcc_data, void *user_data) {
-
+  std::cout << "well hullo!\n";
+  if (gcc_data == NULL) {
+    std::cout << "data is NULL\n";
+  } else {
+    std::cout << "data is not NULL, what is it?\n"; // a tree decl :P true for START and FINISH.
+    tree decl1 = (tree) gcc_data;
+    std::cout << "tree code is: " << TREE_CODE(decl1) << "\n";
+  }
 }
 
 int plugin_init(struct plugin_name_args *plugin_info, struct plugin_gcc_version *version) {
   register_callback("GCCPlugin",
-                    PLUGIN_START_PARSE_FUNCTION,
+                    PLUGIN_FINISH_PARSE_FUNCTION,
                     (plugin_callback_func) handle_function,
                     NULL);
   return 0;
